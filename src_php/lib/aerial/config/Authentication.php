9c5e40c4a34e3dc153b2af2731787363a06ede4d
Danny Kopping <danny@aerial-project.org>
2010-08-18 00:08:02 +0200

    Modified authentication config file to accomodate a better structure Added authentication config parser

--- src_php/lib/aerial/config/Authentication.php
+++ src_php/lib/aerial/config/Authentication.php
@@ -18,7 +18,68 @@
 			$contents = fread($f, filesize($file));
 			fclose($f);
 			
-			self::getInstance()->config = new SimpleXMLElement($contents);
+			$contents = new SimpleXMLElement($contents);
+			self::getInstance()->config = $contents;
+			
+			self::getInstance()->parse();
+		}
+		
+		public function parse()
+		{
+			$contents = $this->config;
+			
+			$acl = array();
+			
+			$groups = $contents->xpath("//group");
+			foreach($groups as $group)
+			{
+				$group = new SimpleXMLElement($group->asXML());
+				
+				$groupArr =& $acl["groups"][(string) $group["name"]];
+				$groupArr = $this->attrToAssoc($group);
+				
+				$users = $group->xpath("(//user)");
+				foreach($users as $user)
+				{
+					$userArr =& $groupArr["users"];
+					$userArr[] = $this->attrToAssoc($user);
+				}
+			}
+			
+			$roles = $contents->xpath("//role");
+			foreach($roles as $role)
+			{
+				$role = new SimpleXMLElement($role->asXML());
+				$roleArr =& $acl["roles"][(string) $role["name"]];
+				$roleArr = $this->attrToAssoc($role);
+				
+				$components = $role->xpath("(//service) | (//function) | (//member)");
+				foreach($components as $compontent)
+				{
+					$name = $compontent->getName()."s";
+					$roleArr[$name][] = $this->attrToAssoc($compontent);
+				}
+			}
+			
+			$rules = $contents->xpath("//rule");
+			foreach($rules as $rule)
+			{
+				$rule = new SimpleXMLElement($rule->asXML());
+				$ruleArr =& $acl["rules"][(string) $rule["name"]];
+				$ruleArr = $this->attrToAssoc($rule);
+			}
+			
+			print_r($acl);	
+			die();
+		}
+		
+		private function attrToAssoc(SimpleXMLElement $node)
+		{
+			$assoc = array();
+			foreach($node->attributes() as $attr => $val)
+				$assoc[$attr] = (string) $val;
+				
+			return $assoc;
 		}
 		
 		public function isValid()

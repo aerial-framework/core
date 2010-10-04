3aa3ff7a3d8a6a7538d49ff8091277f2f748e235
Danny Kopping <danny@aerial-project.org>
2010-10-05 01:34:27 +0200

    Updated Aerial_Record.php to look a bit neater and be a bit more fault tolerant

--- src_php/lib/aerial/utils/Aerial_Record.php
+++ src_php/lib/aerial/utils/Aerial_Record.php
@@ -6,10 +6,37 @@
 		protected function _set($fieldName, $value, $load = true)
 		{
 			if(is_array($value))
-				$value = new Doctrine_Collection("Topic");
+				$value = $this->arrayToCollection($fieldName, $value);
 
-			NetDebug::trace("Set $fieldName to $value");
 			return parent::_set($fieldName, $value, $load);
+		}
+
+		/**
+		 * @param  $fieldName
+		 * @param  $array
+		 * @return Doctrine_Collection|array
+		 *
+		 * Returns a Doctrine_Collection instance if the field key is a relation alias, otherwise return the array
+		 */
+		private function arrayToCollection($fieldName, $array)
+		{
+			$relationKey = $fieldName;
+			$table = $this->getTable();
+
+			try
+			{
+				$relation = $table->getRelation($relationKey);
+			}
+			catch(Doctrine_Table_Exception $e)
+			{
+				return $array;
+			}
+
+			$coll = new Doctrine_Collection($relation->getTable());
+			foreach($array as $post)
+				$coll->add($post);
+
+			return $coll;
 		}
 	}
 ?>

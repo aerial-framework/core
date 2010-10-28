4d7fe1e19e156f2e809e11297d19c46d705dd3b6
Danny Kopping <danny@dannykopping.co.za>
2010-10-28 02:30:58 +0200

    Added sexier exception handling - at present, it handles connection errors, unknown errors, duplicates and referential integrity constraint errors

--- src_php/lib/doctrine/Aerial/Connection/Aerial_Connection.php
+++ src_php/lib/doctrine/Aerial/Connection/Aerial_Connection.php
@@ -1,6 +1,45 @@
 <?php
 	class Aerial_Connection extends Doctrine_Connection_Common
 	{
+		/**
+		 * @var array $modules                      an array containing all modules
+		 *              transaction                 Doctrine_Transaction driver, handles savepoint and transaction isolation abstraction
+		 *
+		 *              expression                  Doctrine_Expression_Driver, handles expression abstraction
+		 *
+		 *              dataDict                    Doctrine_DataDict driver, handles datatype abstraction
+		 *
+		 *              export                      Doctrine_Export driver, handles db structure modification abstraction (contains
+		 *                                          methods such as alterTable, createConstraint etc.)
+		 *              import                      Doctrine_Import driver, handles db schema reading
+		 *
+		 *              sequence                    Doctrine_Sequence driver, handles sequential id generation and retrieval
+		 *
+		 *              unitOfWork                  Doctrine_Connection_UnitOfWork handles many orm functionalities such as object
+		 *                                          deletion and saving
+		 *
+		 *              formatter                   Doctrine_Formatter handles data formatting, quoting and escaping
+		 *
+		 * @see Doctrine_Connection::__get()
+		 * @see Doctrine_DataDict
+		 * @see Doctrine_Expression_Driver
+		 * @see Doctrine_Export
+		 * @see Doctrine_Transaction
+		 * @see Doctrine_Sequence
+		 * @see Doctrine_Connection_UnitOfWork
+		 * @see Doctrine_Formatter
+		 */
+		private $modules = array('transaction' => false,
+								 'expression'  => false,
+								 'dataDict'    => false,
+								 'export'      => false,
+								 'import'      => false,
+								 'sequence'    => false,
+								 'unitOfWork'  => false,
+								 'formatter'   => false,
+								 'util'        => false,
+								 );
+
 		public function connect()
 		{
 			if ($this->isConnected) {
@@ -12,7 +51,7 @@
 			$this->getListener()->preConnect($event);
 
 			$e     = explode(':', $this->options['dsn']);
-			$this->driverName = $e[0]; 
+			$this->driverName = $e[0];
 
 			$found = false;
 
@@ -24,10 +63,27 @@
 
 						$this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 					} catch (PDOException $e) {
-						throw new Aerial_Exception(Aerial_Exception::CONNECTION,
-								array("username" => $this->options['username'],
-										"password" => preg_replace("%.%", "*", $this->options['password'])),
-								$e);
+						switch($e->getCode())
+						{
+							case 2002:						// unresponsive server (server daemon has died)
+								$info = $this->getManager()->parsePdoDsn($this->options["dsn"]);
+								throw new Aerial_Exception(Aerial_Exception::SERVER_UNRESPONSIVE,
+											array("server" => $info["host"],
+													"port" => $info["port"] == null ? "default" : $info["port"]),
+										$e);
+								break;
+							case 1045:
+								throw new Aerial_Exception(Aerial_Exception::CONNECTION,
+										array("username" => $this->options['username'],
+												"password" => preg_replace("%.%", "*", $this->options['password'])),
+										$e);
+								break;
+							default:
+								throw new Aerial_Exception(Aerial_Exception::UNKNOWN,
+										array("message" => $e->getMessage(), "code" => $e->getCode()),
+										$e);
+								break;
+						}
 					}
 					$found = true;
 				}
@@ -81,12 +137,74 @@
 				$e->errorInfo = array(null, null, null, null);
 			}
 			$exc->processErrorInfo($e->errorInfo);
-	
-			 if ($this->getAttribute(Doctrine_Core::ATTR_THROW_EXCEPTIONS)) {
-				throw $exc;
+
+			if ($this->getAttribute(Doctrine_Core::ATTR_THROW_EXCEPTIONS)) {
+
+				$shortError = "";
+				switch($e->errorInfo[1])
+				{
+					case 1452:			// integrity constraint violation - simplify MySQL's cryptic message 
+						$shortError = Aerial_Exception::parseIntegrityConstraint($e->errorInfo[2]);
+						break;
+					default:
+						$shortError = $e->errorInfo[2];
+						break;
+				}
+
+				throw new Aerial_Exception($exc->getPortableMessage(),
+						array("PDO Error Code" => $exc->getCode(),
+								"Database (".$this->driverName.") Error Code" => $e->errorInfo[1],
+								"Short Error" => $shortError,
+								"Full Error" => $exc->getMessage()),
+						$exc);
 			}
 
 			$this->getListener()->postError($event);
+		}
+
+		/**
+		 * __get
+		 * lazy loads given module and returns it
+		 *
+		 * @see Doctrine_DataDict
+		 * @see Doctrine_Expression_Driver
+		 * @see Doctrine_Export
+		 * @see Doctrine_Transaction
+		 * @see Doctrine_Connection::$modules       all availible modules
+		 * @param string $name                      the name of the module to get
+		 * @throws Doctrine_Connection_Exception    if trying to get an unknown module
+		 * @return Doctrine_Connection_Module       connection module
+		 */
+		public function __get($name)
+		{
+			if (isset($this->properties[$name])) {
+				return $this->properties[$name];
+			}
+
+			if(!$this->getDriverName())
+			{
+				$e     = explode(':', $this->options['dsn']);
+				$this->driverName = $e[0];
+			}
+
+			if ( ! isset($this->modules[$name])) {
+				throw new Doctrine_Connection_Exception('Unknown module / property ' . $name);
+			}
+			if ($this->modules[$name] === false) {
+				switch ($name) {
+					case 'unitOfWork':
+						$this->modules[$name] = new Doctrine_Connection_UnitOfWork($this);
+						break;
+					case 'formatter':
+						$this->modules[$name] = new Doctrine_Formatter($this);
+						break;
+					default:
+						$class = 'Doctrine_' . ucwords($name) . '_' . $this->getDriverName();
+						$this->modules[$name] = new $class($this);
+					}
+			}
+
+			return $this->modules[$name];
 		}
 	}
 ?>

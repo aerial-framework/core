c73b2143985f6a5c4dc5ec38914e12e718feeb39
Danny Kopping <danny@dannykopping.co.za>
2010-11-14 20:57:07 +0200

    Reverted PostService.php

--- src_php/org/aerial/services/PostService.php
+++ src_php/org/aerial/services/PostService.php
@@ -4,12 +4,5 @@
 	class PostService extends AbstractService
 	{
 		public $modelName = "Post";
-		
-		public function find($id)
-		{
-			$x = parent::find($id)->toArray();
-			$x["explicitType"] = "org.aerial.vo.Post";
-			return $x;
-		}
 	}
 ?>

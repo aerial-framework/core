bee0145b886b5ee8242610bee4b471559254b7ff
Danny Kopping <dannykopping@gmail.com>
2011-07-24 14:46:14 +0200

    Removed vestigial code

--- src_php/lib/aerial/utils/ModelMapper.php
+++ src_php/lib/aerial/utils/ModelMapper.php
@@ -9,7 +9,7 @@
 				return null;
 			
 			$instance = new $class();
-			
+
 			// Since we're instantiating a new model and then mapping the values, we need to clear out default column values.
 			// Otherwise, Doctrine will assign the default values to "_oldValues" before we finish mapping.
 			foreach($instance->_data as $field=>$value)
@@ -36,34 +36,23 @@
 			}
 
 			$relations = $instance->table->getRelations();
-
-			$mustDelete = false;
-
+			
 			// now assign properties			
 			if(is_array($data) || is_object($data))
 				foreach($data as $key => $value)
 				{
 					if(is_undefined($value))
-						continue;
+							continue;
 					else
 						if($value === null)
 						{
-							$instance->$key = NULL;
-							continue;
+								$instance->$key = NULL;
+								continue;
 						}
-						
+
 					if($instance->table->hasColumn($key) && $value == $instance->table->getDefaultValueOf($key))
-						continue;
-
-					if($key == "deleted")
 					{
-						$mustDelete = true;
-						continue;
-					}
-
-					if($key == "aerialGUID")           // don't add the uid property to the new Doctrine model
-					{
-						$instance->setGUID($value);
+						$instance->$key = $value;
 						continue;
 					}
 	
@@ -76,8 +65,8 @@
 					{
 						$columnName = $instance->getTable()->getColumnName($key);
 						$definition = $instance->getTable()->getColumnDefinition($columnName);
-						
-						// pre-process special types if needed
+
+                        // pre-process special types if needed
 						switch($definition["type"])
 						{
 							case "blob":
@@ -87,69 +76,62 @@
 						}
 						
 						$instance->$key = $value;
-					}			
+					}					
 				}
 
-			if($mustDelete)
-			{
-				$instance->delete();
-			}
-			else
-			{
-				if(is_array($relations) || is_object($relations))
-					foreach($relations as $relation)
+			if(is_array($relations) || is_object($relations))
+				foreach($relations as $relation)
+				{
+					$alias = $relation->getAlias();
+					
+					if(!is_undefined($data[$alias]) && $data[$alias] != null)
 					{
-						$alias = $relation->getAlias();
-
-						if(!is_undefined($data[$alias]) && $data[$alias] != null)
+						$relClass = $relation->getClass();
+						$parentRel = self::hasParentRelationOfType(new $relClass, $class);
+						
+						if($relation->getType() == Doctrine_Relation::MANY)
 						{
-							$relClass = $relation->getClass();
-							$parentRel = self::hasParentRelationOfType(new $relClass, $class);
+							//echo "Many: $alias\n";
+							$collection = new Doctrine_Collection($relation->getClass());
 
-							if($relation->getType() == Doctrine_Relation::MANY)
+							//print_r($data[$alias]);
+							if($data[$alias] instanceof Aerial_ArrayCollection)
 							{
-								//echo "Many: $alias\n";
-								$collection = new Doctrine_Collection($relation->getClass());
-
-								//print_r($data[$alias]);
-								if($data[$alias] instanceof Aerial_ArrayCollection)
-								{
-									foreach($data[$alias]->source as $element)
-									{
-										$childObj = self::mapToModel($relation->getClass(), $element, true);
-										if($parentRel)
-											$childObj->$parentRel = $instance;
-
-										$collection->add($childObj);
-									}
+								foreach($data[$alias]->source as $element)
+								{									
+									$childObj = self::mapToModel($relation->getClass(), $element, true);
+									if($parentRel)
+										$childObj->$parentRel = $instance;
+										
+									$collection->add($childObj);
 								}
-								else
-								{
-									foreach($data[$alias] as $element)
-									{
-										$childObj = self::mapToModel($relation->getClass(), $element, true);
-										if($parentRel)
-											$childObj->$parentRel = $instance;
-
-										$collection->add($childObj);
-									}
-								}
-
-								$instance->$alias = $collection;
 							}
 							else
 							{
-								$childObj = self::mapToModel($relation->getClass(), $data[$alias], true);
-
-								if($parentRel)
-									$childObj->$parentRel = $instance;
-
-								$instance->$alias = $childObj;
+								foreach($data[$alias] as $element)
+								{									
+									$childObj = self::mapToModel($relation->getClass(), $element, true);
+									if($parentRel)
+										$childObj->$parentRel = $instance;
+									
+									$collection->add($childObj);
+								}
 							}
+								
+							$instance->$alias = $collection;
+						}
+						else
+						{
+							$childObj = self::mapToModel($relation->getClass(), $data[$alias], true);
+							
+							if($parentRel)
+								$childObj->$parentRel = $instance;
+								
+							$instance->$alias = $childObj;
 						}
 					}
-			}
-			
+				}
+
 			return self::checkLookup($instance);
 		}
 		
@@ -171,10 +153,8 @@
 		private static function checkLookup($object)
 		{
 			for($x = 0; $x < count(self::$lookup); $x++)
-			{
-				if($object->getGUID() == self::$lookup[$x]->getGUID())
+				if($object->getData() == self::$lookup[$x]->getData())
 					return self::$lookup[$x];
-			}
 
 			self::$lookup[] = $object;
 			return self::$lookup[count(self::$lookup) - 1];

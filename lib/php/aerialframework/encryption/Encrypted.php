d42a067439d35ee6d540384ec7bf522fc5f7280d
Danny Kopping <dannykopping@gmail.com>
2011-07-31 22:39:43 +0200

    Added encryption code from master

---
+++ lib/php/aerialframework/encryption/Encrypted.php
@@ -0,0 +1,10 @@
+<?php
+	class Encrypted
+	{
+		public $_explicitType = "org.aerial.encryption.EncryptedVO";
+
+		// ByteArray containing encrypted data
+		public $data;
+		public $resetKey = false;
+	}
+?>

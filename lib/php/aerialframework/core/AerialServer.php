50977ecb5f5d3ca57a35367e608e00bf8cd00462
Robert Cesaric <robert@cesaric.com>
2011-08-09 14:38:30 -0400

    Removed serialize/unserialize config hooks for config since we're not storing config xml in the class field anymore.

--- lib/php/aerialframework/core/AerialServer.php
+++ lib/php/aerialframework/core/AerialServer.php
@@ -6,18 +6,13 @@ import('aerialframework.core.Bootstrapper');
 
 class AerialServer
 {
-	public $_config;
-	public $_config_alt;
-	
-	public $createDirectories = false;
+	public $createDirectories = false; //ToDo.
 
 	private $startTime;
 
-	/*private $started = false;*/
-	
 	public function __construct()
 	{
-		date_default_timezone_set(ConfigXml::getInstance()->timezone);				// Required for PHP >= 5.3
+		date_default_timezone_set(ConfigXml::getInstance()->timezone); // Required for PHP >= 5.3
 	}
 
 	public function start()
@@ -49,7 +44,7 @@ class AerialServer
 		Bootstrapper::getInstance();
 
 		// Load AMFPHP
-		set_include_path(LIB_PATH . DIRECTORY_SEPARATOR . 'amfphp' . DIRECTORY_SEPARATOR . 'core'); 
+		set_include_path(LIB_PATH . DIRECTORY_SEPARATOR . 'amfphp' . DIRECTORY_SEPARATOR . 'core');
 		import('amfphp.gateway');
 		import('amfphp.globals');
 
@@ -62,27 +57,10 @@ class AerialServer
 		restore_exception_handler();
 	}
 
-	public function __sleep()
-	{
-		$this->_config = $this->_config ? $this->_config->asXML() : null;
-		$this->_config_alt = $this->_config_alt ? $this->_config_alt->asXML() : null;
-
-		return array_keys(get_object_vars($this));
-	}
-
-	public function __wakeup()
-	{
-		$this->_config = $this->_config ? new SimpleXMLElement((string) $this->_config) : null;
-		$this->_config_alt = $this->_config_alt ? new SimpleXMLElement((string) $this->_config_alt) : null;
-
-		return $this;
-	}
-
 	private function startHTMLOutput()
 	{
-		if(AerialStartupManager::hasAMFRequest() )
-		return;
-		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
+		if(!AerialStartupManager::hasAMFRequest() )
+			echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"
 						"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 					<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 
@@ -97,10 +75,8 @@ class AerialServer
 
 	private function endHTMLOutput()
 	{
-		if(AerialStartupManager::hasAMFRequest()) //|| !AerialStartupManager::isDirectCall()
-		return;
-
-		echo "\n\t</body>\n</html>";
+		if(!AerialStartupManager::hasAMFRequest()) //|| !AerialStartupManager::isDirectCall()
+			echo "\n\t</body>\n</html>";
 	}
 
 

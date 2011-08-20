8b12a98e6ac1be83eb357aff58aec9fe4a9d66e9
Robert Cesaric <robert@cesaric.com>
2011-08-20 00:21:36 -0400

    Minor edits to config.php & server.php.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,14 @@
 
 define('CONFIG_PATH', dirname(__FILE__));
 
-if(!@include_once((realpath(CONFIG_PATH . "/../aerial/lib/php/include.php"))))
-	die("Cannot find Aerial library");
+/*
+ * Set the location of include.php
+ * This can normally be found in Aerial's 'lib/php/include.php'
+ */
+$include = realpath(CONFIG_PATH . '/../../Aerial/lib/php/include.php');
 
+if(!file_exists($include))
+	die("Error: cannot find 'include.php' on line 9 of " . __FILE__);
+
+
+include_once($include);
--- server/server.php
+++ server/server.php
@@ -1,12 +1,15 @@
 <?php
 
-$basePath = realpath($_SERVER['DOCUMENT_ROOT']);
+/*
+ * Set the location of config.php.
+ */
+$config = realpath($_SERVER['DOCUMENT_ROOT'] . '/../config/config.php');
 
-if(!@include_once(realpath($basePath . '/../config/config.php')))
-	die("Cannot find config.php");
+if(!file_exists($config))
+	die("Error: cannot find 'config.php' on line 6 of " . __FILE__);
 
+include_once($config);
 import("aerialframework.core.AerialServer");
 
 $server = new AerialServer();
 $server->start();
-
